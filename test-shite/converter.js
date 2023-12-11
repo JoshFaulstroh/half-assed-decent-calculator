16a8e52709c3bbf8f90a6cbcad565dd2fa8589d1
JoshFaulstroh <joshfaulstroh@gmail.com>
2023-12-11 00:54:40 -0600

    progress

---
+++ test-shite/converter.js
@@ -0,0 +1,10 @@
+let inputTxt = document.getElementById('input');
+let basein = document.getElementById('basein');
+let baseout = document.getElementById('baseout');
+let outputTxt = document.getElementById('output');
+
+addEventListener('keyup', function(){
+    let num = +input.value;
+    let base = +basein.value;
+    console.log(num, base)
+})
